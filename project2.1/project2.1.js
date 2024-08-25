caa82e476c66c4081ebb75f42a401c6c33e3d0d8
Zubeydali <160525888+Zubeydali@users.noreply.github.com>
2024-08-25 06:37:51 -0700

    Add files via upload

---
+++ project2.1/project2.1.js
@@ -0,0 +1,50 @@
+// const card= document.querySelector(".card");
+// const card2=document.querySelector(".card1");
+// const btn1= document.querySelectorAll(".btn1");
+// const box= document.querySelectorAll(`#box2.btn1`);
+
+// btn1.forEach(item=>{
+//     item.addEventListener("click",() => {
+// document.querySelector("active").classList.add
+//     })
+// })
+
+
+
+
+// btn1.forEach(item=>{
+//     item.addEventListener("click",()=>{
+//         document.btn1.style.background=""
+//     })
+// })
+
+// btn1.onclick=changeb;
+// function changeb(){
+//     document.btn1.style.background=""
+// }
+
+const card= document.querySelector(".card");
+const card2=document.querySelector(".card1");
+const btn1= document.querySelectorAll(".btn1");
+const box= document.querySelectorAll(`#box2.btn1`);
+
+btn1.forEach(item=>{
+    item.addEventListener("click",() => {
+document.querySelector("active").classList.add
+    })
+})
+
+
+
+
+btn1.forEach(item=>{
+    item.addEventListener("click",()=>{
+        document.btn1.style.background=""
+    })
+})
+
+btn1.onclick=changeb;
+function changeb(){
+    document.btn1.style.background=""
+}
+
